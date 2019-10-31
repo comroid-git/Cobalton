91d992d0aff452814094e01a4aa2763edbde3786
Kaleidox <burdoto@outlook.com>
2019-10-31 01:48:22 +0100

    ffs my windows is broken

--- src/main/java/de/kaleidox/james/command/JamesCommands.java
+++ src/main/java/de/kaleidox/james/command/JamesCommands.java
@@ -1,5 +1,12 @@
 package de.kaleidox.james.command;
 
+import de.kaleidox.javacord.util.commands.Command;
+
 public enum JamesCommands {
     INSTANCE;
+    
+    @Command
+    public String wtf() {
+        return "```";
+    }
 }
