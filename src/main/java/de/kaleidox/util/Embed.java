8e124602589cbb61cf868eefbf2e3da508b4e19f
peanutbother <jg@jabuco-it.de>
2019-10-31 13:12:16 +0100

    make EvalEmbed extend abstract Embed Class

---
+++ src/main/java/de/kaleidox/util/Embed.java
@@ -0,0 +1,29 @@
+package de.kaleidox.util;
+
+import de.kaleidox.JamesBot;
+import de.kaleidox.javacord.util.ui.embed.DefaultEmbedFactory;
+import org.javacord.api.entity.message.embed.EmbedBuilder;
+import org.javacord.api.entity.server.Server;
+import org.javacord.api.entity.user.User;
+
+public abstract class Embed {
+    protected EmbedBuilder embed;
+    public Embed(Server server, User user) {
+        this.embed = DefaultEmbedFactory.create()
+                .setAuthor(user)
+                .setColor(user.getRoleColor(server).orElse(JamesBot.THEME));
+    }
+    public Embed addField(String name, String value) {
+        this.embed.addField(name, value);
+        return this;
+    }
+
+    public Embed addField(String name, String value, boolean inline) {
+        this.embed.addField(name, value, inline);
+        return this;
+    }
+
+    public EmbedBuilder getBuilder() {
+        return this.embed;
+    }
+}
--- src/main/java/de/kaleidox/util/eval/EvalEmbed.java
+++ src/main/java/de/kaleidox/util/eval/EvalEmbed.java
@@ -1,33 +1,15 @@
 package de.kaleidox.util.eval;
 
-import de.kaleidox.JamesBot;
-import de.kaleidox.javacord.util.ui.embed.DefaultEmbedFactory;
-import org.javacord.api.entity.message.embed.EmbedBuilder;
+import de.kaleidox.util.Embed;
 import org.javacord.api.entity.server.Server;
 import org.javacord.api.entity.user.User;
 
-public class EvalEmbed {
-    private EmbedBuilder embed;
+public class EvalEmbed extends Embed {
 
     public EvalEmbed(Server server, User user) {
-        this.embed = DefaultEmbedFactory.create()
-                .setAuthor(user)
+        super(server, user);
+        this.embed
                 .setUrl("http://kaleidox.de:8111")
-                .setFooter("Evaluated by " + user.getDiscriminatedName())
-                .setColor(user.getRoleColor(server).orElse(JamesBot.THEME));
-    }
-
-    public EvalEmbed addField(String name, String value) {
-        this.embed.addField(name, value);
-        return this;
-    }
-
-    public EvalEmbed addField(String name, String value, boolean inline) {
-        this.embed.addField(name, value, inline);
-        return this;
-    }
-
-    public EmbedBuilder getBuilder() {
-        return this.embed;
+                .setFooter("Evaluated by " + user.getDiscriminatedName());
     }
 }
