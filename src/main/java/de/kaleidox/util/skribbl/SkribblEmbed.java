e8a35e6bfff9f76379e8bb9e6d256abb5f856b10
peanutbother <jg@jabuco-it.de>
2019-11-01 05:15:20 +0100

    add skribbl command (in progress)

--- src/main/java/de/kaleidox/util/skribbl/SkribblEmbed.java
+++ src/main/java/de/kaleidox/util/skribbl/SkribblEmbed.java
@@ -13,19 +13,18 @@ public class SkribblEmbed extends Embed {
     //    private final CompletableFuture<Object> result = new CompletableFuture<>();
     public SkribblEmbed(Server server, User user) /*throws URISyntaxException*/ {
         super(server, user);
-        this.embed.setDescription("Bla bli blupp");
-//        SkribblConnector connector = new SkribblConnector();
-//        try {
-//            Object roomResult = connector.getRoom();
-//            if (roomResult instanceof Throwable) {
-//                throw (Throwable) roomResult;
-//            }
-//            HashMap<?, ?> room = (HashMap<?, ?>) roomResult;
-//            this.embed.addField("Skribbl Room", (String) room.get("url"));
-//        } catch (Throwable t) {
-//            //
-//            this.embed.addField("Error", t.getMessage());
-//        }
+        this.embed.setDescription("Lass uns skribbl spielen!");
+        try {
+            SkribblConnector connector = new SkribblConnector();
+            Object roomResult = connector.getRoom();
+            if (roomResult instanceof Throwable) {
+                throw (Throwable) roomResult;
+            }
+            HashMap<?, ?> room = (HashMap<?, ?>) roomResult;
+            this.embed.addField("Skribbl Room", (String) room.get("url"));
+        } catch (Throwable t) {
+            this.embed.addField("Error", t.getMessage());
+        }
     }
 
 //    public boolean complete(Message message) {
