652c0ee436172b6286e24e2e959c3faea40172bc
Tobias Burdow [Kaleidox] <kaleidox@comroid.de>
2020-01-26 04:42:16 +0100

    use isAlphabetic

--- src/main/java/org/comroid/cobalton/engine/AntiSpam.java
+++ src/main/java/org/comroid/cobalton/engine/AntiSpam.java
@@ -47,11 +47,11 @@ public enum AntiSpam implements MessageCreateListener {
             final String content = message.getReadableContent();
 
             long uppercaseCount = content.chars()
-                    .filter(Character::isLetter)
+                    .filter(Character::isAlphabetic)
                     .filter(Character::isUpperCase)
                     .count();
             long lowercaseCount = content.chars()
-                    .filter(Character::isLetter)
+                    .filter(Character::isAlphabetic)
                     .filter(Character::isLowerCase)
                     .count();
 
